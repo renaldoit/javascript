7fb62a2b69f75d355f8a5d633632ceb77b226add
renaldoit <122207239+renaldoit@users.noreply.github.com>
2025-02-19 18:51:19 +0700

    switch

---
+++ switch/switch.js
@@ -0,0 +1,9 @@
+var angka = 2;
+
+if( angka == 1){
+    alert('anda memasukkan angka 1')
+}else if ( angka == 2) {
+    alert('anda memasukkan angka 2')
+}else{
+    alert('angka yang anda masukkan salah')
+}
