15ef43091968426b0660d05e63fc8b43ec64006f
renaldoit <122207239+renaldoit@users.noreply.github.com>
2025-02-21 19:03:56 +0700

    game suitjiwa

--- Latihan Javascript/JavaScript/berlian.js
+++ Latihan Javascript/JavaScript/berlian.js
@@ -1,18 +1,28 @@
 function buatBerlian(tinggi) {
   // Bagian atas berlian
-  for (let i = 1; i <= tinggi; i++) {
-    let spasi = " ".repeat(tinggi - i);
-    let bintang = "*".repeat(2 * i - 1);
+  for ( i = 1; i <= tinggi; i++) {
+     spasi = " ".repeat(tinggi - i);
+     bintang = "*".repeat(3 * i - 1);
     console.log(spasi + bintang);
   }
 
   // Bagian bawah berlian
-  for (let i = tinggi - 1; i >= 1; i--) {
-    let spasi = " ".repeat(tinggi - i);
-    let bintang = "*".repeat(2 * i - 1);
+  for ( i = tinggi - 1; i >= 1; i--) {
+     spasi = " ".repeat(tinggi - i);
+     bintang = "*".repeat(3 * i - 1);
     console.log(spasi + bintang);
   }
 }
 
 // Contoh penggunaan:
 buatBerlian(5); // Tinggi berlian = 5
+
+// buatSegitiga(tinggi) {
+// for ( i = 1; i <= tinggi; i++) {
+//   let spasi = " ", repeat(tinggi - i);
+//   let bintang = "*", repeat(2 * i - 1);
+//   console.log(spasi + bintang);
+
+// }
+// }
+// buatSegitiga(5);
---
+++ Latihan Javascript/JavaScript/suitjawa.js
@@ -0,0 +1,12 @@
+// manangkap pilihan player
+var p = prompt('Silahkan pilih : gajah, semut, orang');
+
+
+// pilihan dari komputer
+// membangkitkan bilangan random
+var comp = Math.random();
+console.log(comp);
+
+// menentukan rules
+
+// menentukan hasil
