dc3283afb4d1cf6c6cb6208f5a6ee5228592123b
renaldoit <122207239+renaldoit@users.noreply.github.com>
2025-02-25 01:06:39 +0700

    live server

---
+++ Latihan Javascript/angkot7/angkot7.js
@@ -0,0 +1,8 @@
+function Angkot(supir, trayek, penumpang, kas) {
+    this.sopir = sopir;
+    this.trayek = trayek;
+    this.penumpang = penumpang;
+    this.kas = kas;
+}
+
+var angkot1 = new Angkot('Sandhika Galih', ['Cicaheum', 'Cibiru'], [], 0);
